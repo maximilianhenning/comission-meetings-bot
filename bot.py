87340fed31761a4ac33696ed3863d38a8fc53d91
Maximilian H <lorganan@gmail.com>
2023-04-20 15:55:01 +0200

    initial commit

---
+++ bot.py
@@ -0,0 +1,9 @@
+# Download new meetings files
+
+# Check if register file needs to be updated and do it if yes
+
+# Go through meetings files, check for new meetings
+
+# Prepare for posting
+
+# Post each new meeting, including register links
